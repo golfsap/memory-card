0bd045d1b0048bcb8bb53374aa0b4f2915befbc4
Golfsap <golf.sap@gmail.com>
2025-03-03 14:07:54 +0700

    modify shuffle algorithm

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,12 @@ function App() {
   const [glow, setGlow] = useState(false);
 
   const shuffleCards = (cardsArray: typeof cardsData) => {
-    return [...cardsArray].sort(() => Math.random() - 0.5);
+    const shuffled = [...cardsArray];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
   };
 
   const handleClick = (cardTitle: string) => {
