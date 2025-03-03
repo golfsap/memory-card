19d3f36a91e435914565b6211bd267b3db1c64aa
Golfsap <golf.sap@gmail.com>
2025-03-03 13:54:38 +0700

    Styling and useEffect shuffle on mount

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Card from "./components/Card";
 import cardsData from "./data";
 import "./App.css";
@@ -8,6 +8,7 @@ function App() {
   const [currentScore, setCurrentScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const [cards, setCards] = useState([...cardsData]);
+  const [glow, setGlow] = useState(false);
 
   const shuffleCards = (cardsArray: typeof cardsData) => {
     return [...cardsArray].sort(() => Math.random() - 0.5);
@@ -18,25 +19,42 @@ function App() {
       setClickedCards((prev) => [...prev, cardTitle]);
       setCurrentScore((prev) => prev + 1);
     } else {
-      setBestScore((prev) => Math.max(prev, currentScore));
+      setBestScore((prev) => {
+        const newBestScore = Math.max(prev, currentScore);
+        if (newBestScore > prev) setGlow(true);
+        return newBestScore;
+      });
       setCurrentScore(0);
       setClickedCards([]);
     }
-    console.log(clickedCards);
-    setCards(shuffleCards(cards));
+    setCards(shuffleCards(cardsData));
   };
+
+  useEffect(() => {
+    setCards(shuffleCards(cardsData));
+    console.log("Shuffling cards on mount");
+  }, []);
+
+  useEffect(() => {
+    if (glow) {
+      const timer = setTimeout(() => setGlow(false), 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [glow]);
 
   return (
     <>
       <header>
-        <h1>Memory card game</h1>
+        <div className="titleContainer">
+          <h1>Memory card game</h1>
+          <p>Rules: don't click the same card twice!</p>
+        </div>
         <div className="scoreContainer">
           <p>Current Score: {currentScore}</p>
-          <p>Best Score: {bestScore}</p>
+          <p className={glow ? "glow" : ""}>Best Score: {bestScore}</p>
         </div>
       </header>
       <section className="cardContainer">
-        {/* <Card /> */}
         {cards.map((card) => (
           <Card
             key={card.name}
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,14 +4,11 @@ interface CardProps {
   onClick: (cardTitle: string) => void;
 }
 
-// const API_KEY = "m8uAzdWsQ3CCT8drxuY7yPvs5O7tW9jD";
-// const trendingURL = `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=5`;
-
 export default function Card({ name, src, onClick }: CardProps) {
   return (
     <div className="card" onClick={() => onClick(name)}>
-      <h2>{name}</h2>
       <img src={src} alt={name}></img>
+      <h2>{name}</h2>
     </div>
   );
 }
